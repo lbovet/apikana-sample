d3efc8fde2ee66cffd1603062570e9cbbeffe570
nidi3 <ich@nidi.guru>
2017-01-25 20:42:31 +0100

    fix example

--- maven/apikana-sample-base/src/model/ts/company.ts
+++ maven/apikana-sample-base/src/model/ts/company.ts
@@ -1,4 +1,4 @@
-interface Company {
+export interface Company {
     id: number
     name: string
 }
--- maven/apikana-sample-dependent/src/model/ts/user.ts
+++ maven/apikana-sample-dependent/src/model/ts/user.ts
@@ -1,4 +1,4 @@
-import {Company} from "Company";
+import {Company} from "apikana-sample-base/company";
 
 interface Users {
     count: number
