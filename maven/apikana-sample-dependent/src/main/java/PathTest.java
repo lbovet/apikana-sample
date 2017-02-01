ba3875ae05a587854ac0f89d9e478f5717e8f7d1
nidi3 <ich@nidi.guru>
2017-02-01 19:20:17 +0100

    test enums, annotated type aliases and model dependency in another package

---
+++ maven/apikana-sample-dependent/src/main/java/PathTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by niederhauste on 31.01.2017.
+ */
+public class PathTest {
+    public static void main(String[] args) {
+//        V1Paths.users.id("www").path();
+    }
+}
--- maven/apikana-sample-dependent/src/model/ts/user.ts
+++ maven/apikana-sample-dependent/src/model/ts/user.ts
@@ -1,15 +1,25 @@
 import {Company} from "apikana-sample-base/company";
-
+import {Int, TimeOnly,DateOnly, DateTime, UTC} from "apikana/default-types";
+export type UUID=string // @format uuid
 interface Users {
-    count: number
+    count: Int
     data: User[]
 }
 
+enum Gender{
+    MALE,FEMALE
+}
+
 interface User {
-    id: number
+    id: UUID
     firstName: string //the given name
     lastName: string //the family name
-    age?: number
+    age?: Int
+
+    birthday: DateOnly
+    birthtime: TimeOnly
+    birth: DateTime
+    birthinstant: UTC
     parent?: User
     company: Company
 }
